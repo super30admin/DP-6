2356a816541ad7ccfea014e728bab0aad0154e5a
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-08-29 21:32:34 -0500

    Completed DP - 6 - Problem 2.
    Implemented both optimal and brute force approach

--- Problem2.java
+++ Problem2.java
@@ -3,7 +3,7 @@
 // Worked on LeetCode : Yes
 
 // Approach :
-// 1. Since first  ugly numbers is . return n if n<=1.
+// 1. Since first  ugly numbers is 1. return n if n<=1.
 // 2. Now have an array of size n. Fill first element with 1 .Have pointer for 2,3,5 on indexes 0,0,0.
 // 3. Now find product of each prime(2,3,5) with dp[i] and find min and update the pointer for which minimum is found.
 //    If multiple primes have same min value update pointers for all primes which have min value . Do this n times and 
@@ -30,5 +30,39 @@ class Solution {
             else    k++;
         }
         return dp[n-1];
+    }
+}
+
+// Brute Force Approach
+
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+// Worked on LeetCode : Time Limit Exceeded
+
+// Approach :
+// 1. Since first  ugly numbers is 1. return n if n<=1.
+// 2. For each integer after 1 , check if it is an ugly number . Divide the number by 2, 3 and 5 . Do this till quotient 
+//    is 1. Else it is not ugly. If it is ugly increment count. Do this till count equals n and return n.
+class Solution {
+    public int nthUglyNumber(int n) {
+        int num = 1;
+        int count = 1;
+        int nugly = 1;
+        while(count<n){
+            num++;
+            if(isUgly(num)) {
+                nugly = num;
+                count++;
+            }
+        }
+        return nugly;
+}
+    public boolean isUgly(int num){
+        if(num == 1)    return true;
+        if(num%2 == 0)  num = num/2;
+        else if(num%3 == 0)  num = num/3;
+        else if(num%5 == 0)  num = num/5;
+        else return false;
+        return isUgly(num);
     }
 }
