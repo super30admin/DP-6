486bc0477324b6a679d5299c92f79b8e1a118cb5
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-26 11:44:48 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -41,3 +41,39 @@ class Solution {
         }
     }
 }
+
+//****New Ugly Number****
+//Time Complexity:
+//Space Complexity:
+//Leetcode runnable : Y;
+//Any doubts: N;
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        PriorityQueue<Long> pq=new PriorityQueue<>();
+        HashSet<Long> set=new HashSet<>();
+        long curr=(long)1;
+        pq.add(curr);
+        set.add(curr);
+        int count=0;
+        int [] primes=new int[] {2,3,5};
+        
+        while(count<n);
+        {
+            curr=pq.poll();
+            count++;
+            
+            for(int prime: primes)
+            {
+                long newugly= prime*curr;
+                if(!set.contains(newugly))
+                {
+                    pq.add(newugly);
+                    set.add(newugly);
+                }
+            }
+            
+        }
+        return (int)curr;
+    }
+}
