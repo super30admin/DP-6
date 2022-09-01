52776b5b23a25a5acc9605bd518b5e607f8f2cb2
Jeel Patel <35517028+jeelpatel18@users.noreply.github.com>
2022-09-01 10:00:45 -0500

    Update UglyNumber2.java

--- UglyNumber2.java
+++ UglyNumber2.java
@@ -24,3 +24,42 @@ class Solution {
         return (int)l;
     }
 }
+
+//TC : O(N)
+//SC : O(1) // constant
+
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        
+        int i = 0, j = 0, k = 0; //Pointers for 2,3,5
+        int v1 = 2, v2 = 3, v3 = 5; //For Values 2,3,5
+        for(int x = 1; x < n; x++){
+            int min = Math.min(v1, Math.min(v2,v3));
+            res[x] = min;
+            
+            if(min == v1){
+                i++;
+                //if(i < n)
+                v1 = res[i] * 2;
+            }
+            if(min == v2){
+                j++;
+                //if(j < n)
+                v2 = res[j] * 3;                
+            }
+            if(min == v3){
+                k++;
+                //if(k < n)
+                v3 = res[k] * 5;                
+            }
+        }
+        
+        
+        return res[n-1];
+    }
+}
+
+
