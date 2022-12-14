66000f2cd24c0ea1384faf46e62812693741e551
ajoshi <ajoshi@cvent.com>
2022-12-13 23:14:24 -0500

    Problems 1 and 2

---
+++ UglyNumber2.java
@@ -0,0 +1,37 @@
+// Time Complexity: O(n)
+// Space Complexity: O(N)
+class Solution {
+    public int nthUglyNumber(int n) {
+        if (n==1) return 1;
+
+        int p2 = 0, p3 = 0, p5 = 0;
+        int n2 = 2, n3 = 3, n5 = 5;
+        int[] result = new int[n];
+        result[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int min = Math.min(n2, Math.min(n3, n5));
+            result[i] = min;
+            if (min == n2) {
+                p2++;
+                System.out.println("p2" + p2);
+                n2 = 2 * result[p2];
+            }
+
+            if (min == n3) {
+                p3++;
+                System.out.println("p3" + p3);
+                n3 = 3 * result[p3];
+            }
+
+            if (min == n5) {
+                p5++;
+                System.out.println("p5" + p5);
+                n5 = 5 * result[p5];
+            }
+
+        }
+
+        return result[n-1];
+    }
+}
